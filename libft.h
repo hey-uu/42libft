cf22ed372657592ce1546833eee995ed995345da
Hyeyun Kim <hyeyukim@c4r8s4.42seoul.kr>
2022-07-14 21:12:49 +0900

    add functs to libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hyeyukim <hyeyukim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 09:34:17 by hyeyukim          #+#    #+#             */
-/*   Updated: 2022/07/14 20:56:08 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2022/07/14 21:12:04 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstadd_front(t_list **lst, t_list *new);
