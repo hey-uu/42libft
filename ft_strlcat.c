ed2f43132f5e93c291e1105a9e500447c0d48258
Hyeyun Kim <hyeyukim@c4r8s4.42seoul.kr>
2022-07-14 21:15:32 +0900

    change ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,58 +6,58 @@
 /*   By: hyeyukim <hyeyukim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 14:53:54 by hyeyukim          #+#    #+#             */
-/*   Updated: 2022/07/13 23:16:40 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2022/07/14 21:15:14 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
 
-// size_t	ft_strlcat(char *dest, const char *src, size_t size)
-// {
-// 	size_t	destlen;
-// 	size_t	srclen;
-// 	size_t	i;
-
-// 	destlen = ft_strlen(dest);
-// 	srclen = ft_strlen(src);
-// 	if (size <= destlen)
-// 		return (srclen + size);
-// 	dest = &dest[destlen];
-// 	i = 0;
-// 	while (i < size - destlen - 1 && src[i])
-// 	{
-// 		dest[i] = src[i];
-// 		i++;
-// 	}
-// 	dest[i] = '\0';
-// 	return (destlen + srclen);
-// }
-#include "libft.h"
-#include "libft.h"
-size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-    size_t  srcsize;
-    size_t  orisize;
-    size_t  i;
-    orisize = 0;
-    srcsize = 0;
-    while (dst[orisize])
-        orisize++;
-    while (src[srcsize])
-        srcsize++;
-    if (dstsize > orisize)
-    {
-        i = 0;
-        while (src[i] && i < (dstsize - orisize - 1))
-        {
-            dst[orisize + i] = src[i];
-            i++;
-        }
-        dst[orisize + i] = '\0';
-        return (orisize + srcsize);
-    }
-    return (dstsize + srcsize);
+	size_t	destlen;
+	size_t	srclen;
+	size_t	i;
+
+	destlen = ft_strlen(dest);
+	srclen = ft_strlen(src);
+	if (size <= destlen)
+		return (srclen + size);
+	dest = &dest[destlen];
+	i = 0;
+	while (i < size - destlen - 1 && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (destlen + srclen);
 }
+// #include "libft.h"
+// #include "libft.h"
+// size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
+// {
+//     size_t  srcsize;
+//     size_t  orisize;
+//     size_t  i;
+//     orisize = 0;
+//     srcsize = 0;
+//     while (dst[orisize])
+//         orisize++;
+//     while (src[srcsize])
+//         srcsize++;
+//     if (dstsize > orisize)
+//     {
+//         i = 0;
+//         while (src[i] && i < (dstsize - orisize - 1))
+//         {
+//             dst[orisize + i] = src[i];
+//             i++;
+//         }
+//         dst[orisize + i] = '\0';
+//         return (orisize + srcsize);
+//     }
+//     return (dstsize + srcsize);
+// }
 /*
 #include <stdio.h>
 #include <string.h>
