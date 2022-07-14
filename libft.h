9380a31b6b0af9a4b26f94fb4d378cb482f7ff0c
Hyeyun Kim <hyeyukim@c4r8s4.42seoul.kr>
2022-07-14 20:55:35 +0900

    add ft_puchar_fd.c & ft_putstr_fd.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hyeyukim <hyeyukim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 09:34:17 by hyeyukim          #+#    #+#             */
-/*   Updated: 2022/07/14 09:17:21 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2022/07/14 20:50:37 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 typedef enum e_bool
 {
@@ -58,14 +59,14 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 
-// void	ft_lstadd_back(t_list **lst, t_list *new);
-// void	ft_lstadd_front(t_list **lst, t_list *new);
-// void	ft_lstclear(t_list **lst, void (*del)(void *));
-// void	ft_lstdelone(t_list *lst, void (*del)(void *));
-// void	ft_lstiter(t_list *lst, void (*f)(void *));
-// t_list	*ft_lstlast(t_list *lst);
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-// t_list	*ft_lstnew(void *content);
-// int		ft_lstsize(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 
 #endif
