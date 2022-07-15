7b250077ac6a9c24d7d6aac9e0e9e92052defdb6
Hyeyun Kim <hyeyukim@c6r9s4.42seoul.kr>
2022-07-15 17:39:33 +0900

    norm error

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: hyeyukim <hyeyukim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 14:30:39 by hyeyukim          #+#    #+#             */
-/*   Updated: 2022/07/13 22:38:03 by hyeyukim         ###   ########.fr       */
+/*   Updated: 2022/07/15 17:38:51 by hyeyukim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	t_list	*tem;
-	
+
 	if (!lst || !f)
 		return ;
 	while (lst)
